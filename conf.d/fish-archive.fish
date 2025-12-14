256813a87158c456be9a7541b6ba7332d3376027
givensuman <givensuman@duck.com>
2025-12-14 16:55:00 -0500

    Better standardizing for names

---
+++ conf.d/fish-archive.fish
@@ -0,0 +1,12 @@
+function _fish_archive_install --on-event fish-archive_install
+end
+
+function _fish_archive_uninstall --on-event fish-archive_uninstall
+    functions --erase compress
+    functions --erase extract
+end
+
+function _fish_archive_update --on-event fish-archive_update
+    _fish_archive_uninstall
+    _fish_archive_install
+end
